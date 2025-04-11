07364ee248590ffe217af3c6caf0350d78713e6b
Nauman Pathan <naumanpathan98239@gmail.com>
2025-04-11 19:29:53 +0530

    fix: setup proxy and dist for render

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,17 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react()],
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'http://localhost:5050', // your backend port locally
         changeOrigin: true,
         secure: false,
       },
     },
+  },
+  build: {
+    outDir: 'dist', // ensure this matches what Render expects
   },
 })
